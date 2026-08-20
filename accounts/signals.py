f71879aef824a74e69684695ac486765a00b1e67
Rohit Vishwakarma <rohitvvishwakarma04@gmail.com>
2026-08-20 10:21:16 +0530

    Update signals.py

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,9 +3,7 @@ from django.dispatch import receiver
 from .models import User
 
 
-# adding this as a safety net - even if someone creates a User outside
-# the serializer (shell, admin panel, whatever), they shouldn't end up
-# with a blank role
+
 @receiver(pre_save, sender=User)
 def set_default_role(sender, instance, **kwargs):
     if not instance.role:
